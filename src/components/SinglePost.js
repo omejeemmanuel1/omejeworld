3212025256a9185e11b4aa1bf119b1e129a47dcc
Omeje Emmanuel Xavier <omejeemmanuel046@yahoo.com>
2022-08-07 10:49:01 +0100

    Padding

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -42,7 +42,7 @@ if (!singlePost) return <div>Loading...</div>;
       <article className="container shadow-lg mx-auto bg-[#f5f5f5] rounded-lg">
         <header className='relative'>
           <div className='absolute h-full w-full flex items-center justify-center p-8'>
-            <div className='bg-white bg-opacity-75 rounded p-12'>
+            <div className='bg-white bg-opacity-75 rounded p-6'>
               <h1 className='font-orbitron text-sm md:text-3xl mb-4'>
                 {singlePost.title}
               </h1>
