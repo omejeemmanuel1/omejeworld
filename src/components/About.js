ea2003012f58cecf6a34a9f68862c421a22a20f0
Omeje Emmanuel Xavier <omejeemmanuel046@yahoo.com>
2022-08-08 17:25:11 +0100

    Add

--- src/components/About.js
+++ src/components/About.js
@@ -9,15 +9,14 @@ const About = () => {
     }, [])
 
   return (
-    <div id="about" name='about' className='w-full overflow-hidden h-[600px] md:h-[500px] bg-[#0a192f]  md:pt-[150px] text-gray-300'>
+    <div id="about" name='about' className='w-full overflow-hidden h-[650px] md:h-[500px] bg-[#0a192f]  md:pt-[150px] text-gray-300'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8 md:-mt-[200px]'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8  md:-mt-[200px]'>
           <div className='sm:text-right pb-8 pl-4'>
             <p className='text-2xl font-bold text-justify inline border-b-4 border-pink-600 font-orbitron'>
               About Me
             </p>
           </div>
-          <div></div>
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='text-left text-4xl font-bold'>
