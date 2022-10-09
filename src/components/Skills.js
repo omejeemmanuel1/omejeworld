610148ed7fa178257eb1b1d8e1e10eae3d6c363d
Omeje Emmanuel Xavier <omejeemmanuel046@yahoo.com>
2022-10-09 05:29:20 -0700

    Remove

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -57,10 +57,6 @@ const Skills = () => {
                   <img className='w-20 mx-auto' src={Mongo} alt="Mongodb icon" />
                   <p className='my-4'>MONGO DB</p>
               </div>
-              <div data-aos="fade-right" className='shadow-md shadow-[#040c16] md:hover:scale-110 duration-500 bg-[#0a192f] '>
-                  <img className='w-20 mx-auto' src={FireBase} alt="Firebase icon" />
-                  <p className='my-4'>FIREBASE</p>
-              </div>
               <div data-aos="fade-left" className='shadow-md shadow-[#040c16] md:hover:scale-110 duration-500 bg-[#0a192f] '>
                   <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
                   <p className='my-4'>TAILWIND</p>
