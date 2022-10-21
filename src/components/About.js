51c89745cdae171c2aff46ea1abaf28bb8bd6623
Omeje Emmanuel Xavier <omejeemmanuel046@yahoo.com>
2022-10-21 06:08:10 -0700

    resume

--- src/components/About.js
+++ src/components/About.js
@@ -32,7 +32,7 @@ const About = () => {
             </div>
           </div>
           <button data-aos="flip-up" className='mt-10 justify-center text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            <a href="../assets/My resume.pdf" download>My Resume</a>
+            <a href="../assets/My resume.docx" download>My Resume</a>
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowDown className='ml-3 ' />
             </span>
