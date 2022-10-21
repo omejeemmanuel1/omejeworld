0811a40b31e227946670a0329f5f4c39132a9a20
Omeje Emmanuel Xavier <omejeemmanuel046@yahoo.com>
2022-10-21 05:22:48 -0700

    Download

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -153,7 +153,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href="../assets/my resume.docx" download>
+              href="../assets/my resume.docx" download="Emmanuel Omeje CVgit ">
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
